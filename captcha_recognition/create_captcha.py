4b475e541a40afe6b9fe876f9d290168896e08ad
guoxiaobo96 <guoxiaobo96@gmail.com>
2017-11-30 22:49:41 -0500

    test

--- captcha_recognition/create_captcha.py
+++ captcha_recognition/create_captcha.py
@@ -8,15 +8,15 @@ from PIL import Image, ImageDraw, ImageFont, ImageFilter
 __FONT_PATH = '\\fonts\\arial.ttf'
 __CAPTCHA_NUMBER = 6
 __CAPTCHA_SIZE = (100, 30)
-__BACK_COLOUR = (255,255,255)
+__BACK_COLOUR = (255, 255, 255)
 #字体颜色，默认为蓝色
-__FONT_COLOUR = (0,0,255)
+__FONT_COLOUR = (0, 0, 255)
 #干扰线颜色。默认为红色
-__LINE_COLOUR = (255,0,0)
+__LINE_COLOUR = (255, 0, 0)
 #是否要加入干扰线
 __DRAW_LINE = True
 #加入干扰线条数的上下限
-__LINE_NUMBER =5
+__LINE_NUMBER = 5
 
 def __gene_text(__captcha_number = None):
     """生成一个随机的字符串，用于产生验证码，验证码的内容包括了大小写字母和阿拉伯数字
@@ -26,23 +26,25 @@ def __gene_text(__captcha_number = None):
     captcha_text返回值是一个字符串，用以生成验证码
     """
     if __captcha_number is None:
-        captcah_number=__CAPTCHA_NUMBER
+        captcah_number = __CAPTCHA_NUMBER
     captcha_source = list(string.ascii_letters+string.digits)
-    captcha_text = ''.join(random.sample(source,__CAPTCHA_NUMBER))
-    return captchat_text
+    captcha_text = ''.join(random.sample(captcha_source, __CAPTCHA_NUMBER))
+    return captcha_text
 
-def __gene_line(draw, width, height):
+def __gene_line(draw, width, height,line_colour=None):
     """
     用来绘制干扰线
-    ARGS: 
+    ARGS:
     draw是一个传入的画笔
     width,height 是整幅图的宽，即规定了干扰线的范围
     RETURN:
     返回值为空
     """
+    if line_colour is None:
+        line_colour = __LINE_COLOUR
     begin = (random.randint(0, width), random.randint(0, height))
     end = (random.randint(0, width), random.randint(0, height))
-    draw.line([begin, end], fill = __LINE_COLOUR)
+    draw.line([begin, end], fill = line_colour)
 
 def gene_code(captcha_size=None, back_colour=None, font_colour=None,line_colour=None, line_number=None):
     """
@@ -65,15 +67,15 @@ def gene_code(captcha_size=None, back_colour=None, font_colour=None,line_colour=
         line_number = __LINE_NUMBER
 
     width, height=captcha_size
-    image = Image.new('RGBA',(width,height),back_colour)
-    font = ImageFont.truetype(__FONT_PATH,25) 
+    image = Image.new('RGBA', (width, height), back_colour)
+    font = ImageFont.truetype(__FONT_PATH, 25) 
     draw = ImageDraw.Draw(image)  
     text = __gene_text() 
     font_width, font_height = font.getsize(text)
-    draw.text(((width - font_width) / captcha_number, (height - font_height) / __captcha_number),text,\
+    draw.text(((width - font_width) / captcha_number, (height - font_height) / __captcha_number), text, \
             font= font,fill=font_colour) 
-    __gene_line(draw,width,height)
-    image = image.transform((width+20,height+10), Image.AFFINE, (1,-0.3,0,-0.1,1,0),Image.BILINEAR)  #创建扭曲
+    __gene_line(draw, width, height)
+    image = image.transform((width+20, height+10), Image.AFFINE, (1, -0.3, 0, -0.1, 1, 0), Image.BILINEAR)  #创建扭曲
     image = image.filter(ImageFilter.EDGE_ENHANCE_MORE) #滤镜，边界加强
     image.save('idencode.png') 
 if __name__ == "__main__":

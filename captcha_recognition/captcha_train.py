6b0a0956c10a0d658e1c99e5741a0210d13fed32
guoxiaobo96 <guoxiaobo96@gmail.com>
2017-12-01 21:36:21 -0500

    test

--- captcha_recognition/captcha_train.py
+++ captcha_recognition/captcha_train.py
@@ -1,13 +1,15 @@
+"""
+通过生成的验证码训练模型，其中图片的规格为（60，160，3）
+采用的训练模型为cnn，当训练准确率达到98%时停止
+"""
 #coding: utf-8
 from create_captcha import generate_text_and_image
-from PIL import image
 
 import numpy as np
 import tensorflow as tf
 import string
 
 captcha_text, captcha_image=generate_text_and_image()
-print(captcha_image.shape())
 CHAPTCHA_LEN=len(captcha_text)
 
 def char2pos(c):
@@ -21,11 +23,11 @@ def char2pos(c):
 def prepare_image(captcha_image):
     if  len(captcha_image.shape()) >2:
         captcha_image=captcha_image.convert("L")
-    np.pad(image,((2,3), (2,2 )),'constant',constant_value = (255, ))
+    return captcha_image
 
 def text2vec(captcha_text):
     char_set = string.ascii_letters+string.digits
-    captcha_vector = np.zeros(len(captcha_text)*char_set)
+    captcha_vector = np.zeros(len(captcha_text)*len(char_set))
     for i,c in enumerate(captcha_text):
         index = i*text_len+char2pos(c)
         captcha_vector[i] = 1
@@ -45,6 +47,17 @@ def vec2text(captcha_vector):
     return captcha_text
 
 def get_next_batch(batch_size):
-    batch_x=np.zeros([batch_size, ])
+    captcha_text, captcha_image=generate_text_and_image()
+    image_height, image_weight, _ =captcha_image.shape
+    batch_x=np.zeros([batch_size, image_height*image_weight])
+    batch_y=np.zeros([batch_size, CHAPTCHA_LEN*len(string.ascii_letters+string.digits)])
+    for i in range (batch_size):
+        captcha_image=prepare_image(captcha_image)
+        batch_x[i, : ]=captcha_image.flatten()/255
+        batch_y[i, : ]=text2vec(captcha_text)
+    x=tf.placeholder(tf.float16, [None, image_height*image_weight])
+    y=tf.placeholder(tf.float16, [None, CHAPTCHA_LEN*len(string.ascii_letters+string.digits)])
+    keep_prob=tf.placeholder(tf.float16)
+
 
 

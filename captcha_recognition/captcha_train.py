37f39a53e3f6b69a021e8be86d772fd9e419fa72
guoxiaobo96 <guoxiaobo96@gmail.com>
2017-12-01 22:01:24 -0500

    test

--- captcha_recognition/captcha_train.py
+++ captcha_recognition/captcha_train.py
@@ -13,19 +13,41 @@ captcha_text, captcha_image = generate_text_and_image()
 CHAPTCHA_LEN = len(captcha_text)
 
 def char2pos(word):
+    """
+    将字符变为一个向量中的位置
+    ARGS:
+    word:待转换的字母
+    RETURNS：
+    k:转换完毕的字母
+    """
     k = ord(word)-48
     if k > 9:
         k = ord(word) - 55
         if k > 35:
             k = ord(word) - 61
+    return k
 
 
 def prepare_image(captcha_image):
+    """
+    对于验证码进行一系列的准备工作，主要是将验证码由彩色变为黑白
+    ARGS：
+    captcha_image：待转换的图片
+    RETURNS：
+    captcha_imgae:转换完毕的图片
+    """
     if  len(captcha_image.shape()) >2:
         captcha_image = captcha_image.convert("L")
     return captcha_image
 
 def text2vec(captcha_text):
+    """
+    将生成的验证码字符转为向量，方便进行运算
+    ARGS：
+    captcha_text:生成的验证码字符
+    RETURN:
+    captcha_vector:转换完毕的验证码字符串
+    """
     char_set = string.ascii_letters+string.digits
     captcha_vector = np.zeros(len(captcha_text)*len(char_set))
     for i,c in enumerate(captcha_text):
@@ -34,6 +56,13 @@ def text2vec(captcha_text):
     return captcha_vector
 
 def vec2text(captcha_vector):
+    """
+    将验证码向量转为字符
+    ARGS：
+    captcha_vector:验证码字符串
+    RETURN:
+    captcha_text:生成的验证码字符
+    """
     char_pos = captcha_vector.nonzero()[0]
     captcha_text = ''
     for _, c in enumerate(char_pos):
@@ -47,6 +76,13 @@ def vec2text(captcha_vector):
     return captcha_text
 
 def get_next_batch(batch_size):
+    """
+    生成用于计算的next_batch
+    ARGS:
+    next_batch:cnn计算时的步长
+    RETURNS：
+    batch_x, batch_y:生成的用于计算的向量
+    """
     captcha_text, captcha_image=generate_text_and_image()
     image_height, image_weight, _ =captcha_image.shape
     batch_x=np.zeros([batch_size, image_height*image_weight])
@@ -55,9 +91,7 @@ def get_next_batch(batch_size):
         captcha_image=prepare_image(captcha_image)
         batch_x[i, : ]=captcha_image.flatten()/255
         batch_y[i, : ]=text2vec(captcha_text)
-    x=tf.placeholder(tf.float16, [None, image_height*image_weight])
-    y=tf.placeholder(tf.float16, [None, CHAPTCHA_LEN*len(string.ascii_letters+string.digits)])
-    keep_prob=tf.placeholder(tf.float16)
+    return batch_x, batch_y
 
 
 
